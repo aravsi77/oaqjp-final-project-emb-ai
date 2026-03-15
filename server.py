9e7fdfa2d0bf1feff32ad64b9f4d8ca4e4923e32
aravsi77 <yudiisvara@gmail.com>
2026-03-15 14:29:35 +0100

    add new server.py file

---
+++ server.py
@@ -0,0 +1,24 @@
+# Import Flask, render_template, request from the flask pramework package
+from flask import Flask, render_template, request
+# Import the emotion detector function from the package created:
+from EmotionDetection.emotion_detection import emotion_detector
+#Initiate the flask app
+app = Flask("Emotion Detector")
+
+@app.route('/emotionDetector')
+def sent_detector():
+    #capture the text from html file
+    text_to_analyze = request.args.get('textToAnalyze')
+    resp = emotion_detector(text_to_analyze)
+    
+    return f"For the given statement, the system response is 'anger': {resp['anger']}, 'disgust': {resp['disgust']}, 'fear': {resp['fear']}, 'joy': {resp['joy']}, 'sadness': {resp['sadness']}. The dominant emotion is {resp['dominant_emotion']}."
+
+@app.route("/")
+def render_index_page():
+    #render static index html
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    #This functions executes the flask app and deploys it on localhost:5000
+    app.run(host="0.0.0.0", port=5000)
