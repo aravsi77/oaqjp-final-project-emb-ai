c056ed3f3a1fe360e2ae657c894b25acdd999246
aravsi77 <yudiisvara@gmail.com>
2026-03-15 11:41:47 +0100

    fix variable name text_to_analyze

--- emotion_detection.py
+++ emotion_detection.py
@@ -6,7 +6,7 @@ def emotion_detector(text_to_analyze):
     #get the date from IBM watson API
     url =  'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    input =  { "raw_document": { "text": text_to_analyse } }
+    input =  { "raw_document": { "text": text_to_analyze } }
     #send the request to ibm Watson and get the response
     response = requests.post(url, json = input, headers = header)
     #format the response to text
